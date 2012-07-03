c3a7a56ea3abd79d175f04bc22e183635d4451c4
Rob Purcell <borllecrup@me.com>
2012-07-03 23:03:37 +0100

    Add support for Gradle wrapper.
    Also CucumberJVM support and sample Java test.  Exclude Idea files, as
    they can be generated from the Gradle script.

---
+++ src/test/java/com/robbyp/escapala/HelloStepDefs.java
@@ -0,0 +1,11 @@
+package com.robbyp.escapala;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rob
+ * Date: 03/07/2012
+ * Time: 22:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloStepDefs {
+}
